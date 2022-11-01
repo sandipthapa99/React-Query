21667a59ce0d02d4df1b38fcae0b7ebd1258da8c
Sandip <sandipthapa@cagtu.com>
2022-11-01 12:40:05 +0545

    Implemented features of caching and staling for lower number of network requests

--- src/App.js
+++ src/App.js
@@ -3,43 +3,49 @@ import { Link, Route, Routes } from "react-router-dom";
 import { SuperHeroesPage } from "./components/Superheroes.page";
 import { RQSuperHeroesPage } from "./components/RQSuperheroes.page";
 import { HomePage } from "./components/Home.page";
-import {QueryClientProvider, QueryClient } from "@tanstack/react-query"
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function App() {
     return (
         <QueryClientProvider client={queryClient}>
-        <div className="App">
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/super-heroes">
-                                Traditional Super Heroes
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/rq-super-heroes">RQ Super Heroes</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Routes>
-                    <Route path="/super-heroes" element={
-                        <SuperHeroesPage />}/>
-                    
-                    <Route path="/rq-super-heroes" element={
-                        <RQSuperHeroesPage />}/>
-                    
-                    <Route path="/" element={
-                        <HomePage />}/>
-                    
-                </Routes>
-            </div>
-        </div>
+            <ReactQueryDevtools/>
+                <div className="App">
+                    <div>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/super-heroes">
+                                        Traditional Super Heroes
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/rq-super-heroes">
+                                        RQ Super Heroes
+                                    </Link>
+                                </li>
+                            </ul>
+                        </nav>
+                        <Routes>
+                            <Route
+                                path="/super-heroes"
+                                element={<SuperHeroesPage />}
+                            />
+
+                            <Route
+                                path="/rq-super-heroes"
+                                element={<RQSuperHeroesPage />}
+                            />
+
+                            <Route path="/" element={<HomePage />} />
+                        </Routes>
+                    </div>
+                </div>
         </QueryClientProvider>
     );
 }
