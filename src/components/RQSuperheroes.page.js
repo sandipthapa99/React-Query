062446ed399de960692e4a3580ed0d824f73fbb5
Sandip <sandipthapa@cagtu.com>
2022-11-01 12:40:05 +0545

    Implemented features of caching for lower number of network requests

--- src/components/RQSuperheroes.page.js
+++ src/components/RQSuperheroes.page.js
@@ -2,11 +2,14 @@ import {useQuery} from "@tanstack/react-query"
 import axios from "axios"
 
 const fetchSuperHeroes = () =>{
-  return axios.get("http://localhost:4000/superheroes1");
+  return axios.get("http://localhost:4000/superheroes");
 }
 
 export const RQSuperHeroesPage = () => {
-  const {isLoading, data, isError, error} = useQuery(['super-heroes'], fetchSuperHeroes)
+  const {isLoading, data, isError, error, isFetching} = useQuery(['super-heroes'], fetchSuperHeroes,{
+    staleTime:30000
+  })
+  console.log(isLoading, isFetching)
 
   if(isLoading){
     return <h2>Loading...</h2>
