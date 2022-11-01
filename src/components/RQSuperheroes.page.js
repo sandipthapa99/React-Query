adb49b44ad46552ce984a0eb613c3ff9e47f6930
Sandip <sandipthapa@cagtu.com>
2022-11-01 11:59:09 +0545

    Fetched data using react query

--- src/components/RQSuperheroes.page.js
+++ src/components/RQSuperheroes.page.js
@@ -1,5 +1,28 @@
+import {useQuery} from "@tanstack/react-query"
+import axios from "axios"
+
+const fetchSuperHeroes = () =>{
+  return axios.get("http://localhost:4000/superheroes");
+}
+
 export const RQSuperHeroesPage = () => {
+  const {isLoading, data} = useQuery(['super-heroes'], fetchSuperHeroes)
+
+  if(isLoading){
+    return <h2>Loading...</h2>
+  }
     return (
-      <div>RQ Super Heroes Page</div>
+      <div>
+        <h2>RQ Super Heroes Page</h2>
+        {
+          data?.data.map((hero)=>{
+            return(
+              <div key={hero.id}>
+                {hero.name}
+              </div>
+            )
+          })
+        }
+      </div>
     )
   }     
