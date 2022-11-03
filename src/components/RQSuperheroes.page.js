6df5d4c08d3edf396eb87a6edccee07f649998e3
Sandip <sandipthapa@cagtu.com>
2022-11-03 10:45:30 +0545

    Custom Query Hook

--- src/components/RQSuperheroes.page.js
+++ src/components/RQSuperheroes.page.js
@@ -1,9 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-
-const fetchSuperHeroes = () => {
-    return axios.get("http://localhost:4000/superheroes");
-};
+import { useSuperHeroesData } from "../hooks/useSuperHeroesData";
 
 export const RQSuperHeroesPage = () => {
     const onSuccess = (data) => {
@@ -14,27 +9,10 @@ export const RQSuperHeroesPage = () => {
         console.log("Perform side effects after encountering Error", error);
     };
 
-    const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
-        ["super-heroes"],
-        fetchSuperHeroes,
-        {
-            // staleTime:30000,
-            // refetchOnMount:true, //default
-            // refetchOnWindowFocus: true, //default
-            // refetchInterval:2000,  //For polling i.e. fetching api data at regular interval, the process pauses if window loses focus
-            // refetchIntervalInBackground: true   //fetches data in regular interval even if window loses focus
-            // enabled: false, //disables api call on component mount
-            // onSuccess: onSuccess, //Callback for success on data fetching
-            // onError: onError, //callback for data fetching error
+    const { isLoading, data, isError, error, isFetching, refetch } =
+        useSuperHeroesData(onSuccess, onError);
 
-            select: (data) => {
-                const superheroesNames = data.data.map((hero) => hero.name);
-                return superheroesNames;
-            }, //change destructured data into an array of superheroes names
-        }
-    );
-
-    // console.log(isLoading, isFetching)
+    console.log("isLoading", isLoading, "isFetching", isFetching);
 
     if (isLoading || isFetching) {
         return <h2>Loading...</h2>;
---
+++ src/hooks/useSuperHeroesData.js
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+    return axios.get("http://localhost:4000/superheroes");
+};
+export const useSuperHeroesData = (onSuccess, onError) => {
+    return useQuery(["super-heroes"], fetchSuperHeroes, {
+        // staleTime:30000,
+        // refetchOnMount:true, //default
+        // refetchOnWindowFocus: true, //default
+        // refetchInterval:2000,  //For polling i.e. fetching api data at regular interval, the process pauses if window loses focus
+        // refetchIntervalInBackground: true   //fetches data in regular interval even if window loses focus
+        // enabled: false, //disables api call on component mount
+        // onSuccess: onSuccess, //Callback for success on data fetching
+        // onError: onError, //callback for data fetching error
+
+        select: (data) => {
+            const superheroesNames = data.data.map((hero) => hero.name);
+            return superheroesNames;
+        }, //change destructured data into an array of superheroes names
+    });
+};
