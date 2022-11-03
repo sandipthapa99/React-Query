b8a2488fb398ecd1a9adfe8a440d0f5053d70d15
Sandip <sandipthapa@cagtu.com>
2022-11-03 10:23:43 +0545

    Success and Error callbacks

--- src/components/RQSuperheroes.page.js
+++ src/components/RQSuperheroes.page.js
@@ -1,43 +1,49 @@
-import {useQuery} from "@tanstack/react-query"
-import axios from "axios"
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-const fetchSuperHeroes = () =>{
-  return axios.get("http://localhost:4000/superheroes");
-}
+const fetchSuperHeroes = () => {
+    return axios.get("http://localhost:4000/superheroes");
+};
 
 export const RQSuperHeroesPage = () => {
-  const {isLoading, data, isError, error, isFetching, refetch} = useQuery(['super-heroes'], fetchSuperHeroes,{
-    // staleTime:30000,
-    // refetchOnMount:true, //default
-    // refetchOnWindowFocus: true, //default
-    // refetchInterval:2000,  //For polling i.e. fetching api data at regular interval, the process pauses if window loses focus
-    // refetchIntervalInBackground: true   //fetches data in regular interval even if window loses focus
+    const onSuccess = (data) => {
+        console.log("Perform side effects after data fetching success", data);
+    };
 
-    enabled:false,   //disables api call on component mount
-  })
+    const onError = (error) => {
+        console.log("Perform side effects after encountering Error", error);
+    };
 
-
-  // console.log(isLoading, isFetching)
-
-  if(isFetching){
-    return <h2>Loading...</h2>
-  }
-  if(isError){
-    return <h2>{error.message}</h2>
-  }
-    return (
-      <div>
-        <h2>RQ Super Heroes Page</h2>
-        <button onClick={refetch}>Fetch Heroes</button>
+    const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
+        ["super-heroes"],
+        fetchSuperHeroes,
         {
-          data?.data.map((hero)=>{
-            return(
-              <div key={hero.id}>
-                {hero.name}
-              </div>
-            )
-          })
+            // staleTime:30000,
+            // refetchOnMount:true, //default
+            // refetchOnWindowFocus: true, //default
+            // refetchInterval:2000,  //For polling i.e. fetching api data at regular interval, the process pauses if window loses focus
+            // refetchIntervalInBackground: true   //fetches data in regular interval even if window loses focus
+            // enabled: false, //disables api call on component mount
+            onSuccess: onSuccess,
+            onError: onError,
         }
-      </div>
-    )
-  }     
+    );
+
+    // console.log(isLoading, isFetching)
+
+    if (isLoading || isFetching) {
+        return <h2>Loading...</h2>;
+    }
+    if (isError) {
+        return <h2>{error.message}</h2>;
+    }
+    return (
+        <div>
+            <h2>RQ Super Heroes Page</h2>
+            <button onClick={refetch}>Fetch Heroes</button>
+            {data?.data.map((hero) => {
+                return <div key={hero.id}>{hero.name}</div>;
+            })}
+        </div>
+    );
+};
