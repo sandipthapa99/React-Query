06756c46dfab21e04483bc32bec26bafe6403a18
Sandip <sandipthapa@cagtu.com>
2022-11-03 09:47:26 +0545

    Implemented data fetch on click feature

--- src/components/RQSuperheroes.page.js
+++ src/components/RQSuperheroes.page.js
@@ -6,16 +6,20 @@ const fetchSuperHeroes = () =>{
 }
 
 export const RQSuperHeroesPage = () => {
-  const {isLoading, data, isError, error, isFetching} = useQuery(['super-heroes'], fetchSuperHeroes,{
-    staleTime:30000,
-    refetchOnMount:true, //default
-    refetchOnWindowFocus: true, //default
-    refetchInterval:2000,  //For polling i.e. fetching api data at regular interval, the process pauses if window loses focus
-    refetchIntervalInBackground: true   //fetches data in regular interval even if window loses focus
-  })
-  console.log(isLoading, isFetching)
+  const {isLoading, data, isError, error, isFetching, refetch} = useQuery(['super-heroes'], fetchSuperHeroes,{
+    // staleTime:30000,
+    // refetchOnMount:true, //default
+    // refetchOnWindowFocus: true, //default
+    // refetchInterval:2000,  //For polling i.e. fetching api data at regular interval, the process pauses if window loses focus
+    // refetchIntervalInBackground: true   //fetches data in regular interval even if window loses focus
 
-  if(isLoading){
+    enabled:false,   //disables api call on component mount
+  })
+
+
+  // console.log(isLoading, isFetching)
+
+  if(isFetching){
     return <h2>Loading...</h2>
   }
   if(isError){
@@ -24,6 +28,7 @@ export const RQSuperHeroesPage = () => {
     return (
       <div>
         <h2>RQ Super Heroes Page</h2>
+        <button onClick={refetch}>Fetch Heroes</button>
         {
           data?.data.map((hero)=>{
             return(
